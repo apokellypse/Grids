fd9b09752e321960a04db3df6bed763504d8bf93
Kelly Yu <euriiko@gmail.com>
2015-01-28 14:43:38 -0500

    main screen all set up yays

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+	$home_link = "index.php";
+	$resume_link = "resume.php";
+	$portfolio_link = "portfolio.php";
+	$bio_link = "bio.php";
+	$blog_link = "blog.php";
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include 'constants.php' ?>
+<!doctype html>
+<html>
+<head>
+	<meta name="author" content="Kelly Yu">
+	<meta name="keywords" content="Kelly Yu, Kelly, Yu, apokellypse, Cornell, student, CS, computer science, animation">
+	<meta name="description" content="Kelly Yu is an undergraduate student studying Computer Science in the College of Engineering at Cornell University">
+	<meta content="True" name="HandheldFriendly">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<title>Kelly Yu</title>
+	<link rel="stylesheet" href="css/style.css">
+	<script type="text/javascript" src="scripts/script.js"></script>
+</head>
+<body>
+	<header>
+		<a href="<?=$home_link?>"><div><h1>Kelly Yu</h1></div></a>
+		<nav>
+			<!-- <a href="<?=$home_link?>"><div>Kelly</div></a> -->
+			<a href="<?=$resume_link?>"><div>Resume</div></a>
+			<a href="<?=$portfolio_link?>"><div>Portfolio</div></a>
+			<a href="<?=$bio_link?>"><div>Bio</div></a>
+			<a href="<?=$blog_link?>"><div>Blog</div></a>
+		</nav>
+	</header>
+</body>
+</html>
